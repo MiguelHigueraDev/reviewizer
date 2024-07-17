071d05080497096f9cec282e684dcc62b2eeb96b
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-07-17 01:08:23 -0400

    refactor: move 'AppIntro' to separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import GameSearch from './components/GameSearch/GameSearch';
 import { useState } from 'react';
 import { GameResult } from './interfaces/GameResult';
 import SelectedGamesModal from './components/SelectedGames/SelectedGamesModal';
+import AppIntro from './components/AppIntro';
 
 export default function Home() {
   const [selectedGames, setSelectedGames] = useState([] as GameResult[]);
@@ -22,35 +23,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen max-w-7xl mx-auto gap-10 items-center justify-center p-24">
       <div className="w-1/2">
-        {/* Name and logo */}
-        <div>
-          <h1 className="text-4xl font-bold">Reviewizer AI 🤖</h1>
-          <p className="font-semibold text-xl">
-            Decide on what game to buy using by consulting our AI connoisseur!
-          </p>
-        </div>
-        {/* How it works */}
-        <div>
-          <h2>How it works</h2>
-          <ol>
-            <li>
-              <div>
-                <h3>Search up to 10 Steam games that you are interested in</h3>
-                <p>You can also enter a store link or an AppID</p>
-              </div>
-            </li>
-            <li>
-              <div>
-                <h3>Select a mode</h3>
-                <p>Summary Mode</p>
-                <p>Contrast Mode</p>
-              </div>
-            </li>
-            <li>
-              <div>The AI will select games for you!</div>
-            </li>
-          </ol>
-        </div>
+       <AppIntro />
       </div>
       <div className="w-1/2">
         <GameSearch
