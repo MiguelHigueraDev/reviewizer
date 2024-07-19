055ddc94b357973f84b57eaaa5ef8f0a17e2ecd4
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-07-18 23:01:09 -0400

    feat: add SummaryList and buttons to show the AI summaries

--- src/app/components/SummarySection/SummaryList.tsx
+++ src/app/components/SummarySection/SummaryList.tsx
@@ -1,30 +1,38 @@
-import { SummaryResponse } from "@/app/interfaces/SummaryResponse";
+'use client';
+import { SummaryResponse } from '@/app/interfaces/SummaryResponse';
+import { useState } from 'react';
+import SummaryButtons from './SummaryButtons';
 
 const SummaryList = ({ summaries }: { summaries: SummaryResponse[] }) => {
+  const [currentSummaryIndex, setCurrentSummaryIndex] = useState(0);
+  const summaryCount = summaries.length;
+
   return (
-    <ol>
-      {summaries.map((summary, index) => (
-        <li key={index}>
-          <div className="p-4 bg-neutral-800 mt-4 rounded-xl">
-            <h3 className="font-bold text-2xl mb-2">{summary.title}</h3>
-            <p className="mb-2">{summary.summary}</p>
-            <h4 className="font-semibold text-lg mb-2">Positives:</h4>
-            <ol className="list-disc ml-6 mb-2">
-              {summary.positive.map((positive, index) => (
-                <li key={index}>{positive}</li>
-              ))}
-            </ol>
-            <h4 className="font-semibold text-lg mb-2">Negatives:</h4>
-            <ol className="list-disc ml-6">
-              {summary.negative.map((negative, index) => (
-                <li key={index}>{negative}</li>
-              ))}
-            </ol>
-          </div>
-        </li>
-      ))}
-    </ol>
-  )
+    summaryCount > 0 && (
+      <div className="mt-4">
+        <SummaryButtons
+          summaries={summaries}
+          currentSummaryIndex={currentSummaryIndex}
+          setCurrentSummaryIndex={setCurrentSummaryIndex}
+        />
+        <div className="p-4 bg-neutral-800 mt-2 rounded-xl">
+          <p className="mb-2">{summaries[currentSummaryIndex].summary}</p>
+          <h4 className="font-semibold text-lg mb-2">Positives:</h4>
+          <ol className="list-disc ml-6 mb-2">
+            {summaries[currentSummaryIndex].positive.map((positive, index) => (
+              <li key={index}>{positive}</li>
+            ))}
+          </ol>
+          <h4 className="font-semibold text-lg mb-2">Negatives:</h4>
+          <ol className="list-disc ml-6">
+            {summaries[currentSummaryIndex].negative.map((negative, index) => (
+              <li key={index}>{negative}</li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default SummaryList;
