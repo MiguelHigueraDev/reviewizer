91fe47a9a0f383f4bd7366ab08b021dcc5bdb17f
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-07-19 00:07:41 -0400

    chore: update selected games button to a darker color

--- src/app/components/SelectedGames/SelectedGamesModalButton.tsx
+++ src/app/components/SelectedGames/SelectedGamesModalButton.tsx
@@ -11,7 +11,7 @@ const SelectedGamesModalButton = ({
   return (
     <button
       onClick={onToggleVisibility}
-      className="fixed w-10 top-4 right-4 z-10 bg-white text-black p-2 font-semibold text-lg rounded-lg hover:bg-blue-500 hover:text-white transition-colors duration-100 ease-in-out"
+      className="fixed w-10 top-4 right-4 z-10 bg-neutral-700 text-white p-2 font-semibold text-lg rounded-lg hover:bg-blue-500 hover:text-white transition-colors duration-100 ease-in-out"
     >
       {selectedGames.length}
     </button>
