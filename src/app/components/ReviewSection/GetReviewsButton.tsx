42d827b10c5f6749db7ff8ba078b0f6dde519639
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-07-18 21:09:10 -0400

    feat: add ReviewsModal to see current reviews

---
+++ src/app/components/ReviewSection/GetReviewsButton.tsx
@@ -0,0 +1,28 @@
+import { GameResult } from '@/app/interfaces/GameResult';
+
+const GetReviewsButton = ({
+  selectedGames,
+  onClick,
+}: {
+  selectedGames: GameResult[];
+  onClick: () => void;
+}) => {
+  return selectedGames.length > 0 ? (
+    <button
+      type="button"
+      onClick={onClick}
+      className="mt-4 mb-2 md:mb-0 p-2 bg-blue-700 hover:bg-blue-600 transition-colors duration-100 w-full rounded-md disabled:opacity-50 disabled:cursor-not-allowed"
+    >
+      Get reviews for {selectedGames.length} {selectedGames.length === 1 ? 'game' : 'games'}
+    </button>
+  ) : (
+    <button
+      className="mt-4 mb-2 md:mb-0 p-2 bg-blue-700 hover:bg-blue-600 transition-colors duration-100 w-full rounded-md disabled:opacity-50 disabled:cursor-not-allowed"
+      disabled
+    >
+      Select at least one game
+    </button>
+  );
+};
+
+export default GetReviewsButton;
