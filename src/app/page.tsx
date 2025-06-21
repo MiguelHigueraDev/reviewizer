3ce1de82ea20bfc79e6b53ba59e56c120cf55ddb
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2025-06-21 19:14:11 -0400

    fix: center elements properly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,7 +113,7 @@ export default function Home() {
       className={`dark min-h-screen max-w-7xl mx-auto p-4 md:p-16 ${
         shouldCenterLayout
           ? "flex items-center justify-center"
-          : "lg:flex lg:items-start lg:gap-10 lg:justify-center"
+          : "lg:flex lg:items-center lg:gap-10 lg:justify-center"
       }`}
     >
       <div className={`${shouldCenterLayout ? "w-full max-w-lg" : "lg:w-1/2"}`}>
@@ -133,7 +133,7 @@ export default function Home() {
       </div>
 
       {(summariesLoading || summaries.length > 0) && (
-        <div className="lg:w-1/2 lg:self-start mt-8 lg:mt-0">
+        <div className="lg:w-1/2 lg:self-center mt-8 lg:mt-0">
           {summariesLoading ? (
             <SummaryListSkeleton />
           ) : (
